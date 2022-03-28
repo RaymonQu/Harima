bce55a2bcb34551dac2589f5b9c5535bcad9e421
RaymonQu <dagon7898@gmail.com>
2022-03-28 01:07:49 -0400

    halfway

--- BattleShips/src/BattleShipGame.java
+++ BattleShips/src/BattleShipGame.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
+
 public class BattleShipGame {
+    private Player player;
+    private Player player2;
+    private Scanner scan;
 
+    public BattleShipGame(){
+        player = new Player();
+        player2 = new Player();
+        scan = new Scanner(System.in);
+    }
 
-    // make hard mode, make ai, make game
+    //make ai, make game
     public void start(){
+        System.out.println("             BATTLE SHIP\n\n\n\n        Press Enter to Start");
+        scan.nextLine();
+        System.out.println("This is your board. Where would you like to place your 2 tile long destroyer?");
+        player.getBoard().createBoard();
+        player.getBoard().printBoard();
+    }
+
+
+
+
+
+    public void shoot(){
 
     }
+
 }
